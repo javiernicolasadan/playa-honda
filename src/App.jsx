a6c1ee3eb3eeb6ba22a1af4cece7a3a52c7ac2ed
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-10-11 18:28:48 +0200

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod, dui at suscipit venenatis, risus felis dictum lectus, in accumsan risus quam non nisi. In nec semper odio. Cras sed turpis porta, accumsan diam vel, pharetra ipsum. Fusce in interdum felis, vel hendrerit orci. Ut id odio hendrerit, fringilla nulla quis, volutpat odio. Morbi fermentum tellus eu facilisis mattis. Mauris vitae tortor sodales, ullamcorper sem a, consectetur odio. Morbi non odio id felis rhoncus fringilla aliquam non mauris. Aenean sagittis venenatis enim sed facilisis. Aenean ultricies mattis tincidunt. Aliquam cursus mollis volutpat. Aenean eu feugiat elit.
+        Nulla facilisi. Vivamus congue, nulla eu rutrum pretium, turpis nisl lobortis augue, quis pellentesque velit mauris eleifend est. Sed in nisl non nisi ultrices elementum vitae non justo. Etiam porta leo at sapien feugiat, eget volutpat libero molestie. Nulla nec enim orci. Nam viverra dolor mollis nisl hendrerit fermentum. In dolor massa, convallis et orci cursus, hendrerit ullamcorper lacus. Praesent sagittis ligula vitae rhoncus condimentum.
+        Suspendisse porttitor, lectus non vestibulum luctus, enim ipsum interdum enim, sit amet varius ante justo vestibulum ante. Donec sagittis, lacus ut porttitor bibendum, urna justo mattis nulla, vel commodo velit felis quis nibh. Nam vel pellentesque dui. In facilisis eu arcu ut accumsan. Aenean lacinia iaculis orci a pharetra. Sed pulvinar ullamcorper dolor, id malesuada enim vehicula et. Vivamus eget dictum urna, in sagittis massa.
+        Morbi elementum sagittis leo et bibendum. Ut nulla leo, tincidunt eu quam non, aliquet mollis nibh. Sed malesuada purus magna, ac tincidunt lectus consectetur sagittis. Sed condimentum blandit congue. Proin gravida mattis erat efficitur gravida. Sed nisl dui, porttitor nec elit a, ultricies ullamcorper magna. Maecenas pharetra eu massa ut luctus.
+        Aenean vehicula auctor hendrerit. Proin at nulla dui. Fusce iaculis congue ipsum, eget dignissim turpis vehicula gravida. In sollicitudin, lorem id cursus porttitor, nisl sem malesuada sem, egestas tempus ante velit finibus leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur quis blandit augue, a rutrum eros. Donec elit lacus, condimentum aliquet sem sodales, ultrices hendrerit risus. Etiam interdum elit a tellus ultricies accumsan. Quisque vitae scelerisque metus. Praesent convallis tortor in felis semper pharetra. Mauris dictum felis vel sem vehicula vestibulum. Maecenas sagittis quis lectus ac commodo.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
