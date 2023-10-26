d954b921e290aef5241cbd5b7fdfff9b8bc486f7
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-10-26 17:53:43 +0200

    logic for availability dates in calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker, { registerLocale } from "react-datepicker"
 import { addDays, addMonths } from "date-fns"
 import es from "../../locales/es";
@@ -8,12 +8,41 @@ import reservations from "./reservations";
 const Calendar = () => {
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
+    const [notAvailability, setNotAvailability] = useState(false)
+
     const onChange = (dates) => {
-    const [start, end] = dates;
-    setStartDate(start);
-    setEndDate(end);
+      const [start, end] = dates;
+      setStartDate(start);
+      setEndDate(end);
   };
+  useEffect(() => {
+    if (startDate && endDate) {
+      availableDays();
+    }
+  }, [startDate, endDate]);
+
+    const areDatesEqual = (date1, date2) => {
+      return (
+        date1.getDate() === date2.getDate() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getFullYear() === date2.getFullYear()
+    );
+  }
+    const availableDays = () => {
+      setNotAvailability(false)
+
+      let currentDate = new Date(startDate);
+      while (currentDate <= endDate) {
+        if (reservations.some(reservationDate => areDatesEqual(reservationDate, currentDate))) {
+          setNotAvailability(true);
+          return
+        }
+      currentDate.setDate(currentDate.getDate() + 1); 
+      }
+    }
+
   registerLocale("es", es);
+
   console.log("fecha inicio", startDate)
   console.log("fecha fin", endDate)
   
@@ -34,7 +63,11 @@ const Calendar = () => {
         selectsRange
         inline
         locale="es"
-        />
+        >
+        <div style={{ color: "red" }} >
+          {notAvailability && <p>Fechas no disponibles, elige otras fechas.</p>}
+        </div>
+        </DatePicker>
     </div>
     </React.Fragment>
   );
