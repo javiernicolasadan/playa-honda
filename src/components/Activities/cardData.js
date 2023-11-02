bf84b014c4da02c684fc953ad8ca11c88f8a5098
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-11-02 20:21:27 +0100

    added / to images from cardData

--- src/components/Activities/cardData.js
+++ src/components/Activities/cardData.js
@@ -7,14 +7,14 @@ const cardData = [
       path: "https://www.ksurflamanga.com/blogksurf/"
     },
     {
-      imageSrc: 'DSCF2892.JPG',
+      imageSrc: '/DSCF2892.JPG',
       title: 'Espacios de ocio.',
       description: 'Cerca del apartamento puedes encontrar espacios de ocio tranquilos para relajarse con una bebida.',
       button: "Miaya Beach",
       path: "https://www.google.com/maps/place/Miaya+Beach/@37.6321939,-0.7383953,17z/data=!4m14!1m7!3m6!1s0xd6322a6caaa3423:0xe9f4920688d617a2!2sPlaya+Honda!8m2!3d37.6319714!4d-0.7355354!16s%2Fg%2F11clwn6l_4!3m5!1s0xd632348a6eec18d:0x1ed95d41c502b088!8m2!3d37.6315502!4d-0.7388016!16s%2Fg%2F11s3ydhp32?entry=ttu"
     },
     {
-      imageSrc: 'DSCF2968.JPG',
+      imageSrc: '/DSCF2968.JPG',
       title: 'Paseos en barco',
       description: 'Puedes dar paseos en barco desde varios puntos del Mar Menor.',
       button: "Chapi Taxi",
@@ -25,7 +25,7 @@ const cardData = [
       path3: "https://www.solazlines.com/index.php/es/rutas-regulares/desde-la-manga/2-mares-isla-grosa"
     },
     {
-     imageSrc: 'DSCF2692.JPG',
+     imageSrc: '/DSCF2692.JPG',
      title: 'Paseos en bicicleta',
      description: 'Puedes dar paseos en barco desde varios puntos del Mar Menor.',
      button:"Sobikes",
