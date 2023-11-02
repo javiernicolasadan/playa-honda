9ace59dd9d332db6673dabe4be75c099e69a8780
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-11-02 18:55:41 +0100

    gallery2 with grid totally fine

--- src/components/Gallery2/Gallery2.jsx
+++ src/components/Gallery2/Gallery2.jsx
@@ -1,23 +1,38 @@
+import { useState } from "react";
 import media from "../../assets/media/media.json"
+import  "./gallery2.css"
+
 
 const Gallery2 = () => {
-    console.log(media)
+    const [selectedImage, setSelectedImage] = useState(null)
     return ( 
         <>
-            <div className="container">
+            <div className="container-galleryTwo">
                 <h1>Galeria</h1>
-                <h1>Galeria</h1>
+                <h2>Galeria</h2>
                 
                 
                 <div className="media-container">
                     { 
                         media.images.map((image, index) => (
-                        <div className="media" key={index}>
+                        <div className="media" key={index} onClick={() => setSelectedImage(image)}>
+
                             <img src={image} alt={`image ${index + 1}`} />
                         </div>
                     ))
                     }
                 </div>
+
+                <div className="popup-media" style={{display: selectedImage? "block" : "none"}}>
+                <span onClick={() => setSelectedImage(null)}>&times;</span>
+                {
+                    selectedImage?
+                    <img src={selectedImage} alt="Selected Image"></img> :
+                    null
+                }
+
+                </div>    
+
             </div>
         </>
      );
