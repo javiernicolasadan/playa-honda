23f1c07ab50a1417c463f90c104d02ac4b35393a
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-11-02 19:36:37 +0100

    added categories to the gallery2 in order to choose pictures and render them by category

--- src/components/Gallery2/Gallery2.jsx
+++ src/components/Gallery2/Gallery2.jsx
@@ -5,18 +5,36 @@ import  "./gallery2.css"
 
 const Gallery2 = () => {
     const [selectedImage, setSelectedImage] = useState(null)
+    const [activeCategory, setActiveCategory] = useState("all")
+
+    const categories = Object.keys(media.categories);
+
+    const imagesByCategory = media.categories;
+
+    const changeCategory = (category) => {
+        setActiveCategory(category);
+        setSelectedImage(null);
+      };
+
     return ( 
         <>
             <div className="container-galleryTwo">
-                <h1>Galeria</h1>
                 <h2>Galeria</h2>
-                
-                
+                <div className="category-buttons">
+                    {categories.map((category, index) => (
+                        <button
+                        key={index}
+                        onClick={() => changeCategory(category)}
+                        className={category === activeCategory ? "active" : ""}
+                        >
+                        {category}
+                        </button>
+                ))}
+                </div>
                 <div className="media-container">
                     { 
-                        media.images.map((image, index) => (
+                        imagesByCategory[activeCategory].map((image, index) => (
                         <div className="media" key={index} onClick={() => setSelectedImage(image)}>
-
                             <img src={image} alt={`image ${index + 1}`} />
                         </div>
                     ))
