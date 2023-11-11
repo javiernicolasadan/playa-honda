b85782abc6028ac7f6deee207279e5643aeedf96
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-11-11 20:31:15 +0100

    rendered info about weather of the current day. and config dotenv and vite for global variable of the api

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from "react"
 
+
 const Weather = () => {
+    console.log(process.env)
     const [description, setDescription] = useState();
-    const [main, setMain] = useState();
     const [temp, setTemp] = useState();
+    const [tempMax, setTempMax] = useState();
+    const [tempMin, setTempMin] = useState();
+    const [humidity, setHumidity] = useState();
+    const [speedWind, setSpeedWind] = useState();
 
     const fetchWeather = async () => {
         try {
-            const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=37.63&lon=-0.84&appid=&units=metric&lang=es")
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.63&lon=-0.84&appid=${process.env.OPENWEATHERMAP_API_KEY}&units=metric&lang=es`)
             if(response.status === 200) {
                 const parsed = await response.json() 
                 setDescription(parsed.weather[0].description)
-                setMain(parsed.weather[0].main)
                 setTemp(parsed.main.temp)
+                setTempMax(parsed.main.temp_max)
+                setTempMin(parsed.main.temp_min)
+                setHumidity(parsed.main.humidity)
+                setSpeedWind(parsed.wind.speed)
+                
              console.log(parsed)
             }
                
@@ -29,10 +38,13 @@ const Weather = () => {
     <>
 
         <div style={{ marginTop: "10rem" }} className="text-center">
-        <h5>El tiempo hoy en Playa Honda:</h5>
-        {main}<br/>
+        <h5>El tiempo hoy en Playa Honda:</h5><br/>
+        
         {description}<br/>
         {temp} ºG<br/>
+        <h7>{tempMin} ºG min - {tempMax} ºG max</h7><br/>
+        {humidity}%- humedad<br/>
+        {speedWind} m/s
         </div>
         
     </>  );
