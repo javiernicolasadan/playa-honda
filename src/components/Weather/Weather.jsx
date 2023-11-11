742684eab670a6a19dfa5f20880a020ed45bbd38
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-11-11 14:15:52 +0100

    rendering some meteorogial info

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Weather = () => {
+    const [description, setDescription] = useState();
+    const [main, setMain] = useState();
+    const [temp, setTemp] = useState();
 
     const fetchWeather = async () => {
         try {
-            const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=37.63&lon=-0.84&appid=")
+            const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=37.63&lon=-0.84&appid=&units=metric&lang=es")
             if(response.status === 200) {
                 const parsed = await response.json() 
-                console.log(parsed)
+                setDescription(parsed.weather[0].description)
+                setMain(parsed.weather[0].main)
+                setTemp(parsed.main.temp)
+             console.log(parsed)
             }
+               
         } catch (error) {
             console.log(error)
         }
@@ -20,12 +27,14 @@ const Weather = () => {
 
     return ( 
     <>
-        <p>weather</p>
-        <p>weather</p>
-        <p>weather</p>
-        <p>weather</p>
-        <p>weather</p>
-        <p>weather</p>
+
+        <div style={{ marginTop: "10rem" }} className="text-center">
+        <h5>El tiempo hoy en Playa Honda:</h5>
+        {main}<br/>
+        {description}<br/>
+        {temp} ºG<br/>
+        </div>
+        
     </>  );
 }
  
