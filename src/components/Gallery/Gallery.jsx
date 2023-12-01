67784d62ddcf630dcc4e346dabf2d97f918d6f72
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2023-12-01 21:13:55 +0100

    more space among pic gallery in responsive

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,60 +1,58 @@
 import { useState } from "react";
-import media from "../../assets/media/media.json"
-import  "./gallery.css"
+import media from "../../assets/media/media.json";
+import "./gallery.css";
 import { Nav } from "react-bootstrap";
 
-
 const Gallery = () => {
-    const [selectedImage, setSelectedImage] = useState(null)
-    const [activeCategory, setActiveCategory] = useState("Todas")
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [activeCategory, setActiveCategory] = useState("Todas");
+  const categories = Object.keys(media.categories);
+  const imagesByCategory = media.categories;
 
-    const categories = Object.keys(media.categories);
+  const changeCategory = (category) => {
+    setActiveCategory(category);
+    setSelectedImage(null);
+  };
 
-    const imagesByCategory = media.categories;
-
-    const changeCategory = (category) => {
-        setActiveCategory(category);
-        setSelectedImage(null);
-      };
-
-    return ( 
-        <>
-            <div className="container-galleryTwo mt-0">
-                <h2>Selecciona una categoría.</h2>
-                <div className="category-buttons flex-wrap g-0">
-                    {categories.map((category, index) => (
-                        <Nav.Link
-                        key={index}
-                        onClick={() => changeCategory(category)}
-                        style={{fontSize:"1.3rem", padding:" 0 0.7rem"}}
-                        >
-                        {category}
-                        </Nav.Link>
-                ))}
-                </div>
-                <div className="media-container">
-                    { 
-                        imagesByCategory[activeCategory].map((image, index) => (
-                        <div className="media" key={index} onClick={() => setSelectedImage(image)}>
-                            <img src={image} alt={`image ${index + 1}`} />
-                        </div>
-                    ))
-                    }
-                </div>
-
-                <div className="popup-media" style={{display: selectedImage? "block" : "none"}}>
-                <span onClick={() => setSelectedImage(null)}>&times;</span>
-                {
-                    selectedImage?
-                    <img src={selectedImage} alt="Selected Image"></img> :
-                    null
-                }
-
-                </div>    
-
+  return (
+    <>
+      <div className="container-galleryTwo mt-0">
+        <h2>Selecciona una categoría.</h2>
+        <div className="category-buttons flex-wrap g-0">
+          {categories.map((category, index) => (
+            <Nav.Link
+              key={index}
+              onClick={() => changeCategory(category)}
+              style={{ fontSize: "1.3rem", padding: " 0 0.7rem" }}
+            >
+              {category}
+            </Nav.Link>
+          ))}
+        </div>
+        <div className="media-container">
+          {imagesByCategory[activeCategory].map((image, index) => (
+            <div
+              className="media"
+              key={index}
+              onClick={() => setSelectedImage(image)}
+            >
+              <img src={image} alt={`image ${index + 1}`} />
             </div>
-        </>
-     );
-}
- 
+          ))}
+        </div>
+
+        <div
+          className="popup-media"
+          style={{ display: selectedImage ? "block" : "none" }}
+        >
+          <span onClick={() => setSelectedImage(null)}>&times;</span>
+          {selectedImage ? (
+            <img src={selectedImage} alt="Selected Image"></img>
+          ) : null}
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default Gallery;
