bd3250fa51dc797083a0f4f8d67f050160d6ea3e
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-09-22 21:51:23 +0200

    booking section enhaced with picture

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -9,7 +9,8 @@ const Booking = ({ language, onLanguageChange }) => {
         style={{ marginTop: "3rem" }}
         className="d-flex justify-content-center align-items-center contact"
       >
-        <h3 style={{ lineHeight: "2", margin: "5rem 2rem" }}>
+      <img src="/calareona.jpg" alt="calareona" className="img-booking"/>
+        <h3 style={{ lineHeight: "2", margin: "5rem 2rem" }} className="text-booking">
           {getTranslation("booking.intro", language)} <br />
           {getTranslation("booking.info", language)} <br />
           <Button variant="info"  size="lg" href="https://www.cartagenacosta.com/alquiler/apartamento-playa-honda-apartamento-brisa-y-mar-en-playa-honda-501514.html">
