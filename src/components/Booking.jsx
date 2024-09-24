177d547a4133a4e16e439d04b689316023059f44
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-09-24 14:20:41 +0200

    change booking section. IFrame added in this section for cartagena costa.com

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -8,9 +8,11 @@ const Booking = ({ language, onLanguageChange }) => {
         style={{ marginTop: "3rem" }}
         className="d-flex justify-content-center align-items-center contact"
       >
-        <img src="/calareona.jpg" alt="calareona" className="img-booking" />
+        {/*
+          <img src="/calareona.jpg" alt="calareona" className="img-booking" />
+      */}
         <h3
-          style={{ lineHeight: "2", margin: "5rem 2rem" }}
+          style={{ lineHeight: "2", margin: "2rem 2rem" }}
           className="text-booking"
         >
           {getTranslation("booking.intro", language)} <br />
@@ -25,6 +27,13 @@ const Booking = ({ language, onLanguageChange }) => {
             </Button>
           </a>
         </h3>
+        <iframe
+      src="https://www.cartagenacosta.com/alquiler/apartamento-playa-honda-apartamento-brisa-y-mar-en-playa-honda-501514.html"
+      width="100%"
+      height="800px"
+      style={{ border: "none" }}
+      title="Booking iframe"
+    ></iframe>
       </div>
     </>
   );
