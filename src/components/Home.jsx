8de589e6658d6652c41132c93d75c4d6b079e9f5
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-10-21 14:32:48 +0200

    solver files deleted

---
+++ src/components/Home.jsx
@@ -0,0 +1,78 @@
+import CarouselComponent from "./Carousel";
+import Grid from "./Grid";
+import Parallax from "./Parallax/Parallax";
+import { Spinner } from "react-bootstrap";
+import { useEffect } from "react";
+import { getTranslation } from "../services/localizationservice";
+
+const Home = ({
+  isLoading,
+  handleLoadingChange,
+  language,
+  onLanguageChange,
+}) => {
+  useEffect(() => {
+    handleLoadingChange(true);
+  }, []);
+
+  setTimeout(() => {
+    handleLoadingChange(false);
+  }, 2000);
+
+  return (
+    <main>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <CarouselComponent />
+          <figure className="logo-container py-3">
+            <img src="/logo-costa-calida-color.png" alt="logo" />
+          </figure>
+          <section className="text-container-big px-5 py-2">
+            <h5 className="text-center">
+              {getTranslation("home.textOne", language)}
+              <br />
+              <br />
+              {getTranslation("home.textTwo", language)}
+              <br />
+              <br />
+              {getTranslation("home.textThree", language)}
+              <br />
+              <br />
+              {getTranslation("home.textFour", language)}
+              <br />
+              <br />
+              {getTranslation("home.textFive", language)}
+              <br />
+              <br />
+              {getTranslation("home.textSix", language)}
+              <br />
+              <br />
+              {getTranslation("home.textSeven", language)}
+            </h5>
+          </section>
+          <section className="text-container-small px-5 py-2">
+            <h6 className="text-center">
+              {getTranslation("home.textEight", language)}
+              <br />
+              <br />
+              {getTranslation("home.textNine", language)}
+              <br />
+              <br />
+              {getTranslation("home.textTen", language)}
+              <br />
+              <br />
+              {getTranslation("home.textEleven", language)}
+            </h6>
+          </section>
+          
+          <Parallax language={language}  />
+          <Grid language={language}  />
+        </>
+      )}
+    </main>
+  );
+};
+
+export default Home;
