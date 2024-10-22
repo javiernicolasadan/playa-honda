c86054cefcce3e41c8a3901979463b39e3feac0a
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-10-22 17:15:05 +0200

    visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,21 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
-import purgecss from '@fullhuman/postcss-purgecss';
+import { visualizer } from 'rollup-plugin-visualizer'; // Importa el plugin correcto
 
-dotenv.config()
+dotenv.config();
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true, // Esto abrirá el análisis del bundle automáticamente después de hacer la build
+      filename: './dist/stats.html', // Archivo donde se guardará el reporte
+      gzipSize: true, // Opción para mostrar el tamaño gzip del bundle
+      brotliSize: true // Opción para mostrar el tamaño brotli del bundle
+    })
+  ],
   define: {
     'process.env': process.env
-  },
-  css: {
-    postcss: {
-      plugins: [
-        purgecss({
-          content: ['./index.html', './src/**/*.jsx'],  // Archivos que revisará
-          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [] // Extrae las clases usadas
-        })
-      ]
-    }
   }
 });
