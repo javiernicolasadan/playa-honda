d5bdff5ebef797486d20baf0b07e375ac7193d19
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-10-22 16:53:50 +0200

    purge css

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import purgecss from '@fullhuman/postcss-purgecss';
 
 dotenv.config()
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': process.env
+  },
+  css: {
+    postcss: {
+      plugins: [
+        purgecss({
+          content: ['./index.html', './src/**/*.jsx'],  // Archivos que revisará
+          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [] // Extrae las clases usadas
+        })
+      ]
+    }
   }
-})
+});
