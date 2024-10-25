c882da7c0985e1658c0082521a2c780ad961fa1d
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-10-25 13:41:50 +0200

    connect histoty for redircto all to home,
    For not have a 404 in the google bot search

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import { visualizer } from 'rollup-plugin-visualizer'; // Importa el plugin correcto
+import history from 'connect-history-api-fallback';
 
 dotenv.config();
 
@@ -17,5 +18,15 @@ export default defineConfig({
   ],
   define: {
     'process.env': process.env
-  }
+  },
+  server: {
+    middlewareMode: true,  // activa el modo de middleware
+    setup: (app) => {
+      app.use(
+        history({
+          index: '/index.html',  // asegura que todas las rutas vayan a index.html
+        })
+      );
+    }
+  },
 });
