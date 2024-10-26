c501d802fcfe51968c1a58d8e924f9291782a799
javiernicolasadan <127655730+javiernicolasadan@users.noreply.github.com>
2024-10-26 14:06:54 +0200

    interval changed to fade in carrousel for th biug

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,39 +2,33 @@ import Carousel from "react-bootstrap/Carousel";
 
 const CarouselComponent = () => {
   return (
-    <>
     <div className="carousel-container">
-    <Carousel>
-        <Carousel.Item interval={15000}>
+      <Carousel fade >
+        <Carousel.Item >
           <img
             src="/BarcoSolaz3_resultado.webp"
             className="imgCarousel shadow-1-strong  mb-4 d-block w-100"
             alt="boats in the sea"
           />
-          <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={15000}>
+        <Carousel.Item >
           <img
             src="/DSCF2864_resultado.webp"
             className="imgCarousel shadow-1-strong  mb-4 d-block w-100"
             alt="sunset in Mar Menor sea"
-            loading="lazy" 
           />
           <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={15000}>
+        <Carousel.Item >
           <img
             src="/carouselOneJPG_resultado.webp"
             className="imgCarousel shadow-1-strong mb-4 d-block w-100"
             alt="Mar Menor´s shore"
-            loading="lazy" 
           />
           <Carousel.Caption></Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
-      
-    </>
   );
 };
 
